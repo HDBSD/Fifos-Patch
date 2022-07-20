3f89c003e56589734deaef529595290508243189
Brad S <6626255+HDBSD@users.noreply.github.com>
2022-07-20 23:12:50 +0100

    Implemented Skill Toggles

--- HDPatch/Dev/DebugUI.cs
+++ HDPatch/Dev/DebugUI.cs
@@ -2,6 +2,11 @@
 using System.Reflection;
 using UnityEngine;
 
+using Fifo.Core;
+using Fifo.Core.Saving;
+using Fifo.Characters;
+
+
 namespace HDMods.Dev
 {
     public class DebugUI 
@@ -9,30 +14,102 @@ namespace HDMods.Dev
 
         static Rect windowRect = new Rect(20, 20, 200, 300);
         static float lvlslider = 0;
-
+        static bool showUI = false;
+        static bool pendingUIStateChanged = false;
 
         public static void OnGUI()
         {
             GUI.Label(new Rect(Screen.width - 130, Screen.height - 20, 130, 20), "HDPatch 0.0.1 - <color=red><b>DEV</b></color>");
 
-            windowRect = GUI.Window(0, windowRect, DoMyWindow, "HDPatch - Dev Menu");
-            
+            if (showUI)
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+                windowRect = GUI.Window(0, windowRect, MainUI, "HDPatch - Dev Menu");
+            }
+
+            if (Input.GetKeyDown(KeyCode.F8) && pendingUIStateChanged == false)
+            {
+                pendingUIStateChanged = true;
+                return;
+            }
+
+            if (pendingUIStateChanged && Input.GetKeyDown(KeyCode.F8) == false)
+            {
+                UnityEngine.Object.FindObjectOfType<MenuGenericManager>().enabled = showUI;
+                pendingUIStateChanged = false;
+                showUI = !showUI;
+                Cursor.visible = showUI;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
         }
 
-        static void DoMyWindow(int windowID)
+        static void MainUI(int windowID)
         {
+
+            
+
             lvlslider = LabelSlider(new Rect(15, 20, 100, 20), lvlslider, 10.0f, "Level ID");
-            if (GUI.Button(new Rect(15, 40, 100, 20), "Load Level"))
+
+
+            if (GUI.Button(new Rect(15, 40, 170, 20), "Load Level"))
             {
-                Fifo.Core.GameManager gm = UnityEngine.Object.FindObjectOfType<Fifo.Core.GameManager>();
-                Fifo.Core.Saving.SaveData save = (Fifo.Core.Saving.SaveData)typeof(Fifo.Core.GameManager).GetField("saveData", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(gm);
+                GameManager gm = UnityEngine.Object.FindObjectOfType<GameManager>();
+                SaveData save = (SaveData)typeof(GameManager).GetField("saveData", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(gm);
 
                 if (save == null)
                 {
-                    typeof(Fifo.Core.GameManager).GetField("saveData", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(gm, new Fifo.Core.Saving.SaveData());
+                    typeof(GameManager).GetField("saveData", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(gm, new SaveData());
                 }
 
-                Fifo.Core.SceneLoader.LoadScene((int)lvlslider);
+                SceneLoader.LoadScene((int)lvlslider);
+            }
+
+            if (GUI.Button(new Rect(15, 60, 170, 20), "Toggle Jump"))
+            {
+                Player Ply = UnityEngine.Object.FindObjectOfType<Player>();
+                PlayerSaveData saveData = (PlayerSaveData)typeof(Player).GetField("saveData", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Ply);
+
+                foreach (SkillSaveData skill in saveData.SkillsData.Skills)
+                {
+                    if (skill.Action == "Jump")
+                        skill.Unlocked = !skill.Unlocked;
+                }
+            }
+
+            if (GUI.Button(new Rect(15, 80, 170, 20), "Toggle Double Jump"))
+            {
+                Player Ply = UnityEngine.Object.FindObjectOfType<Player>();
+                PlayerSaveData saveData = (PlayerSaveData)typeof(Player).GetField("saveData", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Ply);
+
+                foreach (SkillSaveData skill in saveData.SkillsData.Skills)
+                {
+                    if (skill.Action == "DoubleJump")
+                        skill.Unlocked = !skill.Unlocked;
+                }
+            }
+
+            if (GUI.Button(new Rect(15, 100, 170, 20), "Toggle Whirlwind"))
+            {
+                Player Ply = UnityEngine.Object.FindObjectOfType<Player>();
+                PlayerSaveData saveData = (PlayerSaveData)typeof(Player).GetField("saveData", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Ply);
+
+                foreach (SkillSaveData skill in saveData.SkillsData.Skills)
+                {
+                    if (skill.Action == "Whirlwind")
+                        skill.Unlocked = !skill.Unlocked;
+                }
+            }
+
+            if (GUI.Button(new Rect(15, 120, 170, 20), "Toggle Stomp"))
+            {
+                Player Ply = UnityEngine.Object.FindObjectOfType<Player>();
+                PlayerSaveData saveData = (PlayerSaveData)typeof(Player).GetField("saveData", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(Ply);
+
+                foreach (SkillSaveData skill in saveData.SkillsData.Skills)
+                {
+                    if (skill.Action == "Stomp")
+                        skill.Unlocked = !skill.Unlocked;
+                }
             }
 
             GUI.DragWindow(new Rect(0, 0, 10000, 20));
@@ -50,7 +127,6 @@ namespace HDMods.Dev
 
             return sliderValue;
         }
-
 
     }
 }
